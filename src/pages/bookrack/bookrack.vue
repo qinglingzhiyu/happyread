aa84bea2e9689b24d3cd16e033981ea91da1ad11
qinglingzhiyu <1012173543@qq.com>
2019-03-10 15:31:18 +0800

    no message

--- src/pages/bookrack/bookrack.vue
+++ src/pages/bookrack/bookrack.vue
@@ -1,15 +1,65 @@
 <!-- 书架页面-->
 <!-- template source-->
 <template>
-    <div class="bookrack">
+    <div class="book-rack">
+        <!-- 头部 -->
         <div class="rack-header">
-            <open-data type="userAvatarUrl" />
-            <div class="header-container">
-                <!-- <open-data type="userNickName" /> -->
-                <image class="avatar" src=""></image>
-                <span class="tip">新用户限免: 14小时19分钟</span>
+            <div class="rack-avatar">
+                <open-data class="avatar" type="userAvatarUrl"></open-data>
             </div>
-            <div class="header-skip">></div>
+            <div class="header-container">
+                <open-data type="userNickName"></open-data>
+                <div class="header-text">新用户限免: 1小时27分钟</div>
+            </div>
+            <div class="rack-skip">></div>
+        </div>
+
+        <!-- 列表 -->
+        <div class="book-list">
+            <div class="book">
+                <image class="book-bg" src="/static/images/book-img.jpg"></image>
+                <div class="book-content">
+                    <div class="book-title">美女总裁老婆</div>
+                    <div class="book-introduce">更新至第2167章 终于见到你</div>
+                </div>
+                <div class="book-skip">
+                    <div class="update">更新</div>
+                    <div class="time">10小时之前</div>
+                </div>
+            </div> 
+            <div class="book">
+                <image class="book-bg" src="/static/images/book-img.jpg"></image>
+                <div class="book-content">
+                    <div class="book-title">美女总裁老婆</div>
+                    <div class="book-introduce">更新至第2167章 终于见到你</div>
+                </div>
+                <div class="book-skip">
+                    <div class="update">更新</div>
+                    <div class="time">10小时之前</div>
+                </div>
+            </div>
+            <div class="book">
+                <image class="book-bg" src="/static/images/book-img.jpg"></image>
+                <div class="book-content">
+                    <div class="book-title">美女总裁老婆</div>
+                    <div class="book-introduce">更新至第2167章 终于见到你</div>
+                </div>
+                <div class="book-skip">
+                    <div class="update">更新</div>
+                    <div class="time">10小时之前</div>
+                </div>
+            </div>
+            <div class="book">
+                <image class="book-bg" src="/static/images/book-img.jpg"></image>
+                <div class="book-content">
+                    <div class="book-title">美女总裁老婆</div>
+                    <div class="book-introduce">更新至第2167章 终于见到你</div>
+                </div>
+                <div class="book-skip">
+                    <div class="update">更新</div>
+                    <div class="time">10小时之前</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -21,15 +71,11 @@ export default {
     mounted() { },
     data() {
         return {
-
         }
     },
     components: { },
-
     methods: {  },
-
     computed: {  }
-
 }
 </script>
 
